133f6407be0b0a339afdef3b8a03fbcb14fe1f46
theo.fratia <theofratia22@gmail.com>
2025-06-10 15:47:44 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,5 +8,5 @@ app.get('/status', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`API disponible sur http://localhost:${PORT}`);
+  console.log(`API disponible sur http://localhost:${PORT}`); 
 });  
